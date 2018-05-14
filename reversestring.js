3aa07e7d1944b3b29c621e25e90a1b90f6643b18
trentduncan <trent.dunc@gmail.com>
2018-05-14 09:08:09 -0700

    finished reverse string drill

---
+++ reversestring.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const reverseString = (str) => {
+  if (str.length === 0) {
+    return '';
+  }
+
+  return str[str.length - 1] + reverseString(str.slice(0,-1));
+};
+
+console.log(reverseString('hello'));
