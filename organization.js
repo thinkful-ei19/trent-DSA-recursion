7558d20f08e937fcf689271cd1b8db43acfc775b
trentduncan <trent.dunc@gmail.com>
2018-05-14 14:22:05 -0700

    finished recursion versions of all algorithms

--- organization.js
+++ organization.js
@@ -1,15 +1,61 @@
 'use strict';
 
 const org = [
-    {id: 'Zuckerberg', parent: null},
-    {id: 'Mammals', parent: 'Animals'},
-    {id: 'Dogs', parent:'Mammals' },
-    {id: 'Cats', parent:'Mammals' },
-    {id: 'Golden Retriever', parent: 'Dogs'},
-    {id: 'Husky', parent:'Dogs' },
-    {id: 'Bengal', parent:'Cats' }
-]
+  {id: 'Zuckerberg', boss: null},
+  {id: 'Schroepfer', boss: 'Zuckerberg'},
+  {id: 'Schrage', boss: 'Zuckerberg'},
+  {id: 'Sandberg', boss: 'Zuckerberg'},
+  {id: 'Bosworth', boss:'Schroepfer' },
+  {id: 'Zhao', boss:'Schroepfer' },
+  {id: 'Steve', boss:'Bosworth' },
+  {id: 'Kyle', boss:'Bosworth' },
+  {id: 'Andra', boss:'Bosworth' },
+  {id: 'Richie', boss: 'Zhao'},
+  {id: 'Sofia', boss: 'Zhao'},
+  {id: 'Jen', boss: 'Zhao'},
+  {id: 'VanDyck', boss:'Schrage' },
+  {id: 'Swain', boss:'Schrage' },
+  {id: 'Sabrina', boss:'VanDyck' },
+  {id: 'Michelle', boss:'VanDyck' },
+  {id: 'Josh', boss:'VanDyck' },
+  {id: 'Blanch', boss:'Swain' },
+  {id: 'Tom', boss:'Swain' },
+  {id: 'Joe', boss:'Swain' },
+  {id: 'Goler', boss:'Sandberg' },
+  {id: 'Hernandez', boss:'Sandberg' },
+  {id: 'Moissinac', boss:'Sandberg' },
+  {id: 'Kelley', boss:'Sandberg' },
+  {id: 'Eddie', boss:'Goler' },
+  {id: 'Julie', boss:'Goler' },
+  {id: 'Annie', boss:'Goler' },
+  {id: 'Rowi', boss:'Hernandez'},
+  {id: 'Inga', boss:'Hernandez'},
+  {id: 'Morgan', boss:'Hernandez'},
+  {id: 'Amy', boss:'Moissinac'},
+  {id: 'Chuck', boss:'Moissinac'},
+  {id: 'Vinni', boss:'Moissinac'},
+  {id: 'Eric', boss:'Kelley'},
+  {id: 'Ana', boss:'Kelley'},
+  {id: 'Wes', boss:'Kelley'},
+];
 
-const organization = () => {
-
+const organization = (org, boss) => {
+  let node = {};
+  org.filter(employee => employee.boss === boss)
+    .forEach(emp => node[emp.id] = organization(org, emp.id));
+  return node;
 };
+
+
+const structuredOrg = organization(org, null);
+
+const traverseR = (obj, depth) => {
+  let indent = ' '.repeat(depth * 4);
+  Object.keys(obj).forEach(item => {
+    console.log(indent + item);
+    return traverseR(obj[item], depth + 1);
+  });
+};
+
+
+console.log(traverseR(structuredOrg, 0));
