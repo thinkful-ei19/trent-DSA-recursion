546fe2cd9da88478b31b4fa20f6d86d7b955117b
trentduncan <trent.dunc@gmail.com>
2018-05-14 11:52:44 -0700

    added edge case for passing in 0 to binary rep

--- binaryrep.js
+++ binaryrep.js
@@ -2,11 +2,14 @@
 
 const binaryRep = (num) => {
   if (num === 0) {
-    return '';
+    return '0';
   }
   const dividedNum = Math.floor(num/2);
   const remainder = num % 2;
+  if (dividedNum === 0) {
+    return `${remainder}`;
+  }
   return binaryRep(dividedNum) + remainder.toString();
 };
 
-console.log(binaryRep(9));
+console.log(binaryRep(5));
